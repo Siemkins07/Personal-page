2924ac783612e6fec59a5a878abbfe036df359fd
Jacek Pobiarżyn <jacek.pobiarzyn@gmial.com>
2019-06-13 15:49:30 +0200

    add new project (spa)

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 $('#more').on('click', function () {
   $('body, html').animate({
-    scrollTop: $('#about-me').offset().top
+    scrollTop: $('.about-me').offset().top
   }, 1000)
 })
+
+// $(document).on('scroll', function () {
+
+//   const windowHeight = $(window).height()
+//   const scrollValue = $(this).scrollTop();
+//   const $photo = $('.photo')
+//   const fromTop = $photo.offset().top;
+//   const photoHeight = $photo.height()
+
+//   if (scrollValue > fromTop + photoHeight - windowHeight - 120) {
+//     $photo.addClass('active')
+//   }
+
+//   if (scrollValue < 100) {
+//     $('div').removeClass('active')
+//   }
+// })
